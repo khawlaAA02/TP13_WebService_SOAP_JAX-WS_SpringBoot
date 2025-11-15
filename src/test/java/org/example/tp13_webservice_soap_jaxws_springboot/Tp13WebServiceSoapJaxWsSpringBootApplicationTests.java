48f0f6c52b744a28457b41ca51ed1a1b29da293f
khawlaAA02 <khawlaattni02@gmail.com>
2025-11-15 21:04:54 +0100

    first commit

---
+++ src/test/java/org/example/tp13_webservice_soap_jaxws_springboot/Tp13WebServiceSoapJaxWsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp13_webservice_soap_jaxws_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp13WebServiceSoapJaxWsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
